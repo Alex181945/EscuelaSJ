4dd6a5392c76aa1d6fc54afb3bc0b8faadcda5df
albertorbs <alberto.roblesortega@gmail.com>
2018-04-07 20:49:58 -0500

    control del alumno

--- escuela/src/main/java/com/sorjuana/escuela/controlador/controlalumno/alumno.java
+++ escuela/src/main/java/com/sorjuana/escuela/controlador/controlalumno/alumno.java
@@ -1,5 +1,15 @@
 package com.sorjuana.escuela.controlador.controlalumno;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class alumno {
 
+		
+			@GetMapping("/controlalumno")
+			public String menu() {
+				return "controlalum";
+			}
 }
+
+		
