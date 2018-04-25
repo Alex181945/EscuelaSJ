f29a66e8392707c6d6c93a34315fa9a3e8103de3
Alex181945 <alestra55@hotmail.com>
2018-04-25 00:19:48 -0500

    Menu

--- escuela/src/main/java/com/sorjuana/escuela/controlador/menu/Menu.java
+++ escuela/src/main/java/com/sorjuana/escuela/controlador/menu/Menu.java
@@ -2,14 +2,18 @@ package com.sorjuana.escuela.controlador.menu;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 
 public class Menu {
 	
 	@GetMapping("/menu")
-	public String menu() {
-		return "prueba";
+	public ModelAndView menu() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("prueba");
+		mav.addObject("menu", true);
+		return mav;
 	}
 
 }
