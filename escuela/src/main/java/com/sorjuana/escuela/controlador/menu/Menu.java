0b68226257ca4db1d9378c2cab305076f6e870d5
Alex181945 <alestra55@hotmail.com>
2018-04-25 22:43:52 -0500

    Cambios en local

--- escuela/src/main/java/com/sorjuana/escuela/controlador/menu/Menu.java
+++ escuela/src/main/java/com/sorjuana/escuela/controlador/menu/Menu.java
@@ -2,10 +2,11 @@ package com.sorjuana.escuela.controlador.menu;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-
+@SessionAttributes("Persona")
 public class Menu {
 	
 	@GetMapping("/menu")
