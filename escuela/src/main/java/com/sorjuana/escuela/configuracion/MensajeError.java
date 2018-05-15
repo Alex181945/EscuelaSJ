161304af5909a14c0d11271a29b5b6dddcc962b5
Alex181945 <alestra55@hotmail.com>
2018-05-14 20:46:59 -0500

    Menu Dinamico

---
+++ escuela/src/main/java/com/sorjuana/escuela/configuracion/MensajeError.java
@@ -0,0 +1,12 @@
+package com.sorjuana.escuela.configuracion;
+
+public class MensajeError {
+	
+	private final static String ERROR1 = "El servicio rest presenta problemas intente mas tarde";
+
+	public static String getERROR1() {
+		return ERROR1;
+	}
+
+
+}
---
+++ escuela/src/main/java/com/sorjuana/escuela/repositorio/modulo/imp/ImpMenuRest.java
@@ -0,0 +1,135 @@
+package com.sorjuana.escuela.repositorio.modulo.imp;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sorjuana.escuela.configuracion.MensajeError;
+import com.sorjuana.escuela.configuracion.VariablesEntorno;
+import com.sorjuana.escuela.modelo.datos.Validacion;
+import com.sorjuana.escuela.modelo.datos.consulta.DosParametrosEnteros;
+import com.sorjuana.escuela.modelo.modulo.Menu;
+import com.sorjuana.escuela.repositorio.modulo.MenuRest;
+
+/**
+ * 
+ * Autor: Alejandro Estrada
+ * Fecha: 07/04/2018
+ * Descripcion: Implementacion de la interfaz MenuRest
+ *  
+ * Modificaciones:
+ * <Quien modifico:> <Cuando modifico:> <Donde modifico:>
+ * Ejemplo: Alejandro Estrada 09/09/2017 In-15 Fn-19 
+ *
+ * Nota: 0 es falso, 1 es verdadero
+ * 
+ */
+
+@Component
+public class ImpMenuRest implements MenuRest {
+	
+	private final static Logger LOGGER = Logger.getLogger(ImpMenuRest.class.getName());
+	private Boolean resultadoLocal;
+	private String  mensajeLocal;
+
+	@SuppressWarnings("static-access")
+	@Override
+	public Menu[] cargaMenu(DosParametrosEnteros consulta, String cToken) {
+		
+		RestTemplate restTemplate = new RestTemplate();		
+		
+		Menu[]       menu         = null;
+		Validacion[] validacion   = null;
+		ObjectMapper mapper       = new ObjectMapper();
+		JsonNode     root         = null;
+		JsonNode     validacionJs = null;
+		JsonNode     datos        = null;
+		
+		try {
+			
+			HttpHeaders headers = new HttpHeaders();
+			headers.add(VariablesEntorno.getHeaderString(), VariablesEntorno.getTokenPrefix() + cToken);
+			
+			MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();     
+			body.add("iTipoConsulta", consulta.getParametro1().toString());
+			body.add("iIDPerfil", consulta.getParametro2().toString());
+
+			HttpEntity<?> httpEntity = new HttpEntity<Object>(body, headers);
+			
+			/*JSON obtenido de forma plana*/
+			ResponseEntity<String> response = restTemplate.exchange(VariablesEntorno.getUrlwsd() + "/carga/modulo",
+					HttpMethod.POST ,httpEntity, String.class);
+			
+			root = mapper.readTree(response.getBody());
+			
+			/*Maneja los errores del servicio rest*/
+			if(root.has("error")) {
+				this.setResultadoLocal(true);
+				this.setMensajeLocal(MensajeError.getERROR1());
+				this.LOGGER.log(Level.SEVERE,root.path("error").toString());
+				return menu = Menu.menuDefault();
+			}
+			
+			validacionJs = root.path("validacion");
+			datos = root.path("datos");
+			
+			validacion = mapper.convertValue(validacionJs, Validacion[].class);
+			
+			if(validacion[0].getlError() == 1) {
+				this.setResultadoLocal(true);
+				this.setMensajeLocal(validacion[0].getcSqlState()+" "+validacion[0].getcError());
+			} else {
+				menu = mapper.convertValue(datos, Menu[].class);
+				this.setResultadoLocal(false);
+				this.setMensajeLocal("");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			this.setResultadoLocal(true);
+			this.setMensajeLocal("Error: Exception en " + new Object() {
+			}.getClass().getEnclosingMethod().getName());
+		}
+
+		return menu;
+	}
+
+	@Override
+	public boolean islResultado() {
+
+		return this.getResultadoLocal();
+	}
+
+	@Override
+	public String getMensaje() {
+
+		return this.getMensajeLocal();
+	}
+
+	public Boolean getResultadoLocal() {
+		return resultadoLocal;
+	}
+
+	public void setResultadoLocal(Boolean resultadoLocal) {
+		this.resultadoLocal = resultadoLocal;
+	}
+
+	public String getMensajeLocal() {
+		return mensajeLocal;
+	}
+
+	public void setMensajeLocal(String mensajeLocal) {
+		this.mensajeLocal = mensajeLocal;
+	}
+
+}
