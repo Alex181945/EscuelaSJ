9a33664e1e1b0157c89e6d11d03a3127bf06d031
albertorbs <alberto.roblesortega@gmail.com>
2018-05-14 23:14:06 -0500

    listo bebe

---
+++ escuela/src/main/java/com/sorjuana/escuela/modelo/ct/Alumno.java
@@ -0,0 +1,16 @@
+package com.sorjuana.escuela.modelo.ct;
+
+public class Alumno {
+	private Integer iPersona;
+	private Integer iIDTipoPersona;
+	private String cNombre;
+	private String cAPaterno;
+	private String cAMaterno;
+	private Integer lGenero;
+	private String dtFechaNac;
+	private Integer lActivo;
+	private String dtCreado;
+	private String dtModificado;
+	
+
+}
