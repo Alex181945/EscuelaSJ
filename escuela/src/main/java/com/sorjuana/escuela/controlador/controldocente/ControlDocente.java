6f15285d58e8eb66c3f6b4bc67d7dcbe2c0d6031
Alex181945 <alestra55@hotmail.com>
2018-05-16 00:05:02 -0500

    OK

--- escuela/src/main/java/com/sorjuana/escuela/controlador/controldocente/ControlDocente.java
+++ escuela/src/main/java/com/sorjuana/escuela/controlador/controldocente/ControlDocente.java
@@ -1,27 +1,43 @@
 package com.sorjuana.escuela.controlador.controldocente;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sorjuana.escuela.configuracion.Vistas;
 import com.sorjuana.escuela.modelo.ct.Docente;
+import com.sorjuana.escuela.modelo.datos.consulta.DosParametrosEnteros;
+import com.sorjuana.escuela.modelo.seg.Usuario;
+import com.sorjuana.escuela.repositorio.modulo.MenuRest;
 
 @Controller
 @SessionAttributes("Persona")
 public class ControlDocente {
 	
+	@Autowired
+	private MenuRest menuRest;
+	
 		@GetMapping("/catalogo/docente/inserta")
-		public ModelAndView inserta() {
+		public ModelAndView inserta(@ModelAttribute("Persona") Usuario sesionPersona) {
+			DosParametrosEnteros consulta = new DosParametrosEnteros();
+			consulta.setParametro1(1); // Tipo de Consulta 0 inactivos, 1 activos, 2 ambos
+			consulta.setParametro2(sesionPersona.getiIDTipoPersona());
+			
 			ModelAndView mav = new ModelAndView();
 			mav.setViewName(Vistas.getFormulariodocente());
-			mav.addObject("menu", true);
+			mav.addObject("menu", menuRest.cargaMenu(consulta, sesionPersona.getcToken()));
 			return mav;
 		}
 		
 		@GetMapping("/controldocente")
-		public ModelAndView menu() {
+		public ModelAndView menu(@ModelAttribute("Persona") Usuario sesionPersona) {
+			DosParametrosEnteros consulta = new DosParametrosEnteros();
+			consulta.setParametro1(1); // Tipo de Consulta 0 inactivos, 1 activos, 2 ambos
+			consulta.setParametro2(sesionPersona.getiIDTipoPersona());
+			
 			ModelAndView mav = new ModelAndView();
 			mav.setViewName(Vistas.getControldocente());
 			Docente doc = new Docente();
@@ -52,7 +68,7 @@ public class ControlDocente {
 			Docente[] docen = {doc, doc1};
 
 			mav.addObject("controld", docen);
-			mav.addObject("menu", true);
+			mav.addObject("menu", menuRest.cargaMenu(consulta, sesionPersona.getcToken()));
 			return mav;
 		}
 		
