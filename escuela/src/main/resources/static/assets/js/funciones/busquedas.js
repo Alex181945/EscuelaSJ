ab8982afc16b9da53b7ccb9f84e9d82bb46cf90b
Alex181945 <alestra55@hotmail.com>
2018-05-24 01:26:39 -0500

    Busqueda

--- escuela/src/main/resources/static/assets/js/funciones/busquedas.js
+++ escuela/src/main/resources/static/assets/js/funciones/busquedas.js
@@ -7,10 +7,10 @@ function busquedaAlumnoKardex(){
 	var numeroAlumno = $('#numAlum').val();
 	var nombreAlumno = $('#nombreAlum').val();
 
-	if(numeroAlumno != "" || numeroAlumno != null ){
+	if(numeroAlumno != "" && numeroAlumno != null ){
 		tipoBusqueda = "numero";
 		elementoBusqueda = numeroAlumno;
-	}else if( nombreAlumno != "" ||  nombreAlumno != null ){
+	}else if( nombreAlumno != "" &&  nombreAlumno != null ){
 		tipoBusqueda = "caracter";
 		elementoBusqueda =  nombreAlumno;
 	}
@@ -23,10 +23,21 @@ function busquedaAlumnoKardex(){
 		 	},
 		 	type : 'POST',
 		 	success : function(data) {	 		 		 		 		
-		 	/*	ListVehiculos(data);	 */
-		 		console.log(data);
-		 		
-		 		if(data == "" || data != "success"){
+		 	/*	ListVehiculos(data);	 */		 		
+		 		if(data == ""){
+		 			
+		 			var dataMod = [];
+		 			var control;
+		 			
+		 			for ( var usuario in data) {
+						console.log(usuario);
+						control = usuario.iPersona;
+						if(usuario.iPersona != usuario.iPersona){
+							dataMod.push(usuario);
+						}
+					}
+		 			
+		 			listaAlumno(dataMod);
 		 			
 		 		}
 		 		
@@ -39,4 +50,50 @@ function busquedaAlumnoKardex(){
 		 	}
 
 		});
+}
+
+function listaAlumno(dataSet){
+	
+	
+	$('#Lista').DataTable( {
+        data: dataSet,
+        "columns" : [
+        	{ "data" : "iPersona" },
+            { "data" : "cNombre" + "cAPaterno" + "cAMaterno" },
+            {   "data": null,
+                "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {                                  	
+                	$(nTd).html(" <button type='button' id='btnEliminarCli' class='btn btn-danger' >" + "<span class='fa fa-trash'  aria-hidden='true'></span> </button>");
+                }
+            },
+			
+		],
+		"pageLength": 5,
+		 "lengthMenu": [ 5 , 10 ],
+	
+		 "language": {
+
+				"sProcessing":     "Procesando...",
+				"sLengthMenu":     "Mostrar _MENU_ registros",
+				"sZeroRecords":    "No se encontraron resultados",
+				"sEmptyTable":     "Ningún dato disponible en esta tabla",
+				"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+				"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+				"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+				"sInfoPostFix":    "",
+				"sSearch":         "Buscar:",
+				"sUrl":            "",
+				"sInfoThousands":  ",",
+				"sLoadingRecords": "Cargando...",
+				"oPaginate": {
+					"sFirst":    "Primero",
+					"sLast":     "Último",
+					"sNext":     "Siguiente",
+					"sPrevious": "Anterior"
+				},
+				"oAria": {
+					"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+					"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+				}
+		 }
+    } );	
 }
